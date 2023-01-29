90942a2b9a0d07d8d4e7fea994571a12ffe7d128
MrHarvyson <josecmoreno3d@gmail.com>
2023-01-29 12:42:08 +0100

    consola imagen termiando

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from jugador import Jugador
 from supertopo import Supertopo
+from colorama import init, Fore, Back, Style
 
 movimientoPlayer = ""
 nombre = input('Hola, ¿Cómo te llamas?: ')
@@ -23,15 +24,15 @@ for topo in lista_topos:
     cadena = "".join(y)
 
 print(cadena)
-
-while jugador.get_casilla() <= 30:
+perder = False
+while jugador.get_casilla() <= 30 and perder == False:
 
     try:
         movimientoPlayer = input(nombre + ' , ¿Cúanto quieres avanzar, 1 o 2 pasos?: ')
         cadena = "______________________________"
         if int(movimientoPlayer) == 1 or int(movimientoPlayer) == 2:
-            # print(jugador.mover(int(movimientoPlayer)))
-            jugador.mover(int(movimientoPlayer))
+            print(jugador.mover(int(movimientoPlayer)))
+            #jugador.mover(int(movimientoPlayer))
             l = list(cadena)
             l[jugador.get_casilla() - 1] = 'X'
             cadena = "".join(l)
@@ -39,15 +40,21 @@ while jugador.get_casilla() <= 30:
             for topo in lista_topos:
                 if 0 < topo.get_sitio() < 30:
                     print(topo.mover())
-                    # topo.mover()
+                    #topo.mover()
                     f = list(cadena)
                     f[topo.get_sitio() - 1] = 'O'
                     cadena = "".join(f)
                     # print(cadena)
                     if jugador.get_casilla() == topo.get_sitio():
+                        t = list(cadena)
+                        t[jugador.get_casilla() - 1] = 'T'
+                        cadena = "".join(t)
+                        print(cadena)
                         print('Perdiste')
+                        perder = True
                         break
-            print(cadena)
+            if perder == False:
+                print(cadena)
             cadena = "______________________________"
         else:
             print("Debe ser 1 o 2")
